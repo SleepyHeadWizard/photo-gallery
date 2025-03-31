3ba4858a82e1b35e5b48698f290e9089f454ab44
SleepyHeadWizard <shiwaditya.chandra@gmail.com>
2025-03-31 19:33:44 +0530

    Implement photo deletion feature with action sheet confirmation

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -105,6 +105,26 @@ export class PhotoService {
       return await this.convertBlobToBase64(blob) as string;
     }
   }
+
+  public async deletePicture(photo: UserPhoto, position: number) {
+    // Remove this photo from the Photos reference data array
+    this.photos.splice(position, 1);
+  
+    // Update photos array cache by overwriting the existing photo array
+    Preferences.set({
+      key: this.PHOTO_STORAGE,
+      value: JSON.stringify(this.photos)
+    });
+  
+    // delete photo file from filesystem
+    const filename = photo.filepath
+                        .substr(photo.filepath.lastIndexOf('/') + 1);
+  
+    await Filesystem.deleteFile({
+      path: filename,
+      directory: Directory.Data
+    });
+  }
   
   private convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
     const reader = new FileReader();
